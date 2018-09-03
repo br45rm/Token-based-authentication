e85845c6fce00657bcc5f14ed46edaff06f1b098
Evgenii Ovsiannikov <br4in5torm@apples-mbp.gateway.sonic.net>
2018-09-03 15:41:05 -0700

    Auth router and controller, mongo/mongoose hooked up,  user model created, bcrypt password encryption added

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+
+// DB Setup
+mongoose.connect(
+  'mongodb://localhost:27017/auth',
+  { useNewUrlParser: true }
+);
 
 // App setup
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 // Server setup
 const port = process.env.PORT || 3090;
