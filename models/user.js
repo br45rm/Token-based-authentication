29ac30dac2279b6f1d33b63638d0038b1dc130b4
Evgenii Ovsiannikov <br4in5torm@apples-mbp.gateway.sonic.net>
2018-09-03 17:19:37 -0700

    bcrypt commented

--- models/user.js
+++ models/user.js
@@ -9,17 +9,23 @@ const userSchema = new Schema({
 });
 
 // On Save Hook, encrypt password
-
+// Before saving a model run this function
 userSchema.pre('save', function(next) {
+  // get access to user model
   const user = this;
+
+  // generate a salt then run callback
   bcrypt.genSalt(10, function(err, salt) {
     if (err) {
       return next(err);
     }
+    // hash our password using the salt
     bcrypt.hash(user.password, salt, null, function(err, hash) {
       if (err) {
         return next(err);
       }
+
+      // overwrite plaintext password with encrypted password
       user.password = hash;
       next();
     });
